bf052531f021a6c3d7c1a67d7507a95fe2c39834
Yantao Xie <xieyt1029@thundersoft.com>
2014-04-08 14:26:38 +0800

    Adjust to make the caller has more control options.

--- toolbox/emacsinit.m
+++ toolbox/emacsinit.m
@@ -1,20 +1,39 @@
-function emacsinit(clientcommand)
-% EMACSINIT Initialize the current MATLAB session for matlab-shell-mode
+function emacsinit(clientcommand, withGraphicalDebugging)
+% EMACSINIT Initialize the current MATLAB session for matlab-shell-mode.
+%     Defaultly, the builtin editor is disable and the graphical debugging is enable.
+% 
+%     EMACSINIT('Default') Enable the builtin editor.
+% 
+%     EMACSINIT('', 'true') Use the default client editor and enable the
+%     graphical debuggin.
+% 
     
-    if usejava('jvm')
-        % Disable built-in editor showing up for debugging
-        com.mathworks.services.Prefs.setBooleanPref('EditorGraphicalDebugging', false);
-        % Disable the built-in editor for normal editing
-        com.mathworks.services.Prefs.setBooleanPref('EditorBuiltinEditor', false);
-        % Use emacsclient no-wait to send edit requests to a
-        % running emacs.
-        if nargin == 0
-            clientcommand = 'emacsclient -n';
+%% Parset argmuments.
+    withBuiltinEditor = false;
+    withGraphicalDebugging = true;
+    % Use emacsclient no-wait to send edit requests to a runnig emacs.
+    defaultClientEditorCommand = 'emacsclient -n';
+    if nargin >= 1
+        if strcmp(clientcommand, lower('Defualt')) == 1
+            withBuiltinEditor = true;
+        elseif strcmp(clientcommand, '') == 1
+            clientcommand = defaultClientEditorCommand;
         end
-        com.mathworks.services.Prefs.setStringPref('EditorOtherEditor', clientcommand);
+    end
+  
+    %% Configure the matlab. 
+    if usejava('jvm')
+        % Enable/Disable built-in editor showing up for debugging.
+        com.mathworks.services.Prefs.setBooleanPref('EditorGraphicalDebugging', withGraphicalDebugging);
+        % Enable/Disable the build-in editor.
+        com.mathworks.services.Prefs.setBooleanPref('EditorBuiltinEditor', withBuiltinEditor);
+        if ~withBuiltinEditor
+            % Set the command for the client editor. 
+            com.mathworks.services.Prefs.setStringPref('EditorOtherEditor', clientcommand)
+        end
     end
 
-    % Use the desktop hotlinking system in MATLAB Shell.  matlab-shell
+    %% Use the desktop hotlinking system in MATLAB Shell.  matlab-shell
     % will interpret them, and provide clickable areas.
     % NOTE: This doesn't work in all cases where HotLinks are used.
     feature('HotLinks','on');
